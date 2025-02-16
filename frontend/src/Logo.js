c34a79a9e1315affe4e1e8e793cdf21ce680a735
chanis10 <isabellec.chan@mail.utoronto.ca>
2025-02-16 03:00:09 -0500

    logo change

--- frontend/src/Logo.js
+++ frontend/src/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.png'; // Make sure to place your logo image in the same directory or update the path accordingly
+import logo from './logo2.png'; // Make sure to place your logo image in the same directory or update the path accordingly
 import './Logo.css';
 
 const Logo = () => {
