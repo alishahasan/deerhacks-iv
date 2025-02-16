f8690c6a51010e2c1b1478103b231fdbae5c8424
chanis10 <isabellec.chan@mail.utoronto.ca>
2025-02-15 19:23:30 -0500

    logo

---
+++ frontend/src/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.png'; // Make sure to place your logo image in the same directory or update the path accordingly
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img src={logo} alt="Logo" className="logo" />
+    </div>
+  );
+};
+
+export default Logo;
